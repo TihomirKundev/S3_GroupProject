fd5f31c6f891ca0045601b97d77818178d17ce0d
Stanchev,Rosen R <--global>
2021-11-26 15:12:20 +0200

    Week13 assignment

---
+++ Week13Assignment/Week13Assignment/Products.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week13Assignment
+{
+    class Products
+    {
+        private string productName="";
+        private double productPrice = 0;
+
+        public void initialiseExpense(string productName,double productPrice)
+        {
+            this.productName = productName;
+            this.productPrice = productPrice;
+        }
+       
+        public string getInfo()
+        {
+            return productName + " - " + productPrice;
+        }
+    }
+}
